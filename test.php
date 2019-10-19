60281693e94789e667de73a28e2d3082d8bcbf55
John Internet <jonjon1234.github@gmail.com>
2019-10-19 23:22:32 +0100

    Add a quick test file.

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$emb = new \Livaco\EasyDiscordWebhook\RichEmbed();
+$emb->setColor('#FF00FF')
+	->setFooter('Footer', 'https://cdn1.iconfinder.com/data/icons/social-media-vol-1-1/24/_github-512.png')
+	->setAuthor('John Internet', 'https://doctor-internet.dev', 'https://doctor-internet.dev/img/logo.png')
+	->addField('Test', 'No u')
+	->addField('Test 2', 'No u2', true)
+	->addField('Test 3', 'No poo', true);
+
+var_dump($emb->jsonSerialize());

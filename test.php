52bdb5eb0578dc22f2b0d427e16daa9a3684420c
John Internet <jonjon1234.github@gmail.com>
2019-10-19 23:39:48 +0100

    Updated test script for generating readme. Updated readme.

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php
 
+use Livaco\EasyDiscordWebhook\RichEmbed;
+use Livaco\EasyDiscordWebhook\WebhookPayload;
+use Livaco\EasyDiscordWebhook\DiscordWebhook;
+
 require __DIR__ . '/vendor/autoload.php';
 
 $emb = new \Livaco\EasyDiscordWebhook\RichEmbed();
@@ -10,9 +14,22 @@ $emb->setColor('#FF00FF')
 	->addField('Test 2', 'No u2', true)
 	->addField('Test 3', 'No poo', true);
 
-$whpl = new \Livaco\EasyDiscordWebhook\WebhookPayload();
-$whpl->addEmbed($emb);
-$whpl->setContent('asdbasdasd');
+$message = new WebhookPayload();
+$message->setContent("This is a test of the emergency webhook system.");
 
-$wh = new \Livaco\EasyDiscordWebhook\DiscordWebhook($_SERVER['DISCORD_URL']);
-$wh->execute($whpl);
+$wh = new DiscordWebhook($_SERVER['DISCORD_URL']);
+$wh->execute($message);
+
+$embed = new RichEmbed();
+$embed->setColor('#FF00FF');
+$embed->setFooter('This is a footer');
+$embed->setAuthor('Livaco', 'https://livaco.dev', 'https://cdn1.iconfinder.com/data/icons/social-media-vol-1-1/24/_github-512.png');
+$embed->addField('Test', 'No u');
+$embed->addField('Test 2', 'No u2', true);
+$embed->addField('Test 3', 'No through', true);
+
+$message = new WebhookPayload();
+$message->addEmbed($embed);
+
+$webhook = new DiscordWebhook($_SERVER['DISCORD_URL']); // Create the Webhook object.
+$webhook->execute($message);
